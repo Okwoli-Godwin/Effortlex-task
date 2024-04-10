3ec6fae64f5598d1e0d659513a48c1c55d55b7f6
Okwoli-Godwin <okwolig60@gmail.com>
2024-04-10 15:18:27 +0100

    service mobile done

--- src/components/ServicespageSection/CoreServices.tsx
+++ src/components/ServicespageSection/CoreServices.tsx
@@ -11,29 +11,29 @@ const CoreServices = () => {
             <h3 className='text-[22px] font-[700] text-[#434141]'>OUR CORE SERVICES</h3>
             
             <div className='w-[100%] flex flex-wrap justify-between mt-[20px]'>
-                <div className='w-[360px] h-[370px] rounded-tr-[15px] rounded-bl-[15px] relative flex justify-center items-center overflow-hidden mb-[50px]'>
+                <div className='w-[360px] md:w-[320px] sm:w-[100%] h-[370px] rounded-tr-[15px] rounded-bl-[15px] relative flex justify-center items-center overflow-hidden mb-[50px]'>
                     <Image src={img} alt='background image' className='w-full h-[100%] object-cover'/>
-                    <h3 className='text-white font-[700] text-[27px] text-center absolute'>RESEARCH & DEVELOPMENT</h3>
+                    <h3 className='text-white font-[700] text-[27px] sm:text-[20px] text-center absolute'>RESEARCH & DEVELOPMENT</h3>
                 </div>
-                <div className='w-[360px] h-[370px] rounded-tr-[15px] rounded-bl-[15px] relative flex justify-center items-center overflow-hidden mb-[50px]'>
+                <div className='w-[360px] md:w-[320px] sm:w-[100%] h-[370px] rounded-tr-[15px] rounded-bl-[15px] relative flex justify-center items-center overflow-hidden mb-[50px]'>
                     <Image src={img2} alt='background image' className='w-full h-[100%] object-cover'/>
-                    <h3 className='text-white font-[700] text-[27px] text-center absolute'>PERSONNEL TRAINNING & WORKSHOP</h3>
+                    <h3 className='text-white font-[700] text-[27px] sm:text-[20px] text-center absolute'>PERSONNEL TRAINNING & WORKSHOP</h3>
                 </div>
-                <div className='w-[360px] h-[370px] rounded-tr-[15px] rounded-bl-[15px] relative flex justify-center items-center overflow-hidden mb-[50px]'>
+                <div className='w-[360px] md:w-[320px] sm:w-[100%] h-[370px] rounded-tr-[15px] rounded-bl-[15px] relative flex justify-center items-center overflow-hidden mb-[50px]'>
                     <Image src={img3} alt='background image' className='w-full h-[100%] object-cover'/>
-                    <h3 className='text-white font-[700] text-[27px] text-center absolute'>RECRUITMENT CONSULTING</h3>
+                    <h3 className='text-white font-[700] text-[27px] sm:text-[20px] text-center absolute'>RECRUITMENT CONSULTING</h3>
                 </div>
-                <div className='w-[360px] h-[370px] rounded-tr-[15px] rounded-bl-[15px] relative flex justify-center items-center overflow-hidden mb-[30px]'>
+                <div className='w-[360px] md:w-[320px] sm:w-[100%] h-[370px] rounded-tr-[15px] rounded-bl-[15px] relative flex justify-center items-center overflow-hidden mb-[30px]'>
                     <Image src={img3} alt='background image' className='w-full h-[100%] object-cover'/>
-                    <h3 className='text-white font-[700] text-[27px] text-center absolute'>RECRUITMENT CONSULTING</h3>
+                    <h3 className='text-white font-[700] text-[27px] sm:text-[20px] text-center absolute'>RECRUITMENT CONSULTING</h3>
                 </div>
-                <div className='w-[360px] h-[370px] rounded-tr-[15px] rounded-bl-[15px] relative flex justify-center items-center overflow-hidden mb-[30px]'>
+                <div className='w-[360px] md:w-[320px] sm:w-[100%] h-[370px] rounded-tr-[15px] rounded-bl-[15px] relative flex justify-center items-center overflow-hidden mb-[30px]'>
                     <Image src={img3} alt='background image' className='w-full h-[100%] object-cover'/>
-                    <h3 className='text-white font-[700] text-[27px] text-center absolute'>RECRUITMENT CONSULTING</h3>
+                    <h3 className='text-white font-[700] text-[27px] sm:text-[20px] text-center absolute'>RECRUITMENT CONSULTING</h3>
                 </div>
-                <div className='w-[360px] h-[370px] rounded-tr-[15px] rounded-bl-[15px] relative flex justify-center items-center overflow-hidden mb-[30px]'>
+                <div className='w-[360px] md:w-[320px] sm:w-[100%] h-[370px] rounded-tr-[15px] rounded-bl-[15px] relative flex justify-center items-center overflow-hidden mb-[30px]'>
                     <Image src={img3} alt='background image' className='w-full h-[100%] object-cover'/>
-                    <h3 className='text-white font-[700] text-[27px] text-center absolute'>RECRUITMENT CONSULTING</h3>
+                    <h3 className='text-white font-[700] text-[27px] sm:text-[20px] text-center absolute'>RECRUITMENT CONSULTING</h3>
                 </div>
             </div>
         </div>
