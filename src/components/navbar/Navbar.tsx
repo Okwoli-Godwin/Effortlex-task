21710922a4c908d5bf2633141d11e0415ca4d2c2
Okwoli-Godwin <okwolig60@gmail.com>
2024-04-10 16:00:03 +0100

    corrections

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -13,6 +13,11 @@ const Navbar = () => {
     const Toggle = () => {
         setShow(!show)
     }
+
+    const closeSidebar = () => {
+        setShow(false);
+    };
+
   return (
     <div className='w-[100%] h-[70px] flex items-center justify-center fixed bg-white z-20'>
         <div className='w-[90%] flex items-center h-[100%] justify-between'>
@@ -31,13 +36,13 @@ const Navbar = () => {
             <div onClick={Toggle} className='text-[23px] cursor-pointer hidden sm:flex'><FiMenu /></div>
         </div>
 
-        <div className={`fixed z-10 top-[70px] right-0 w-[100%] h-[100vh] bg-rgba ${show ? "translate-x-0" : "translate-x-full"}`}>
+        <div className={`fixed z-30 top-[70px] right-0 w-[100%] h-[100vh] bg-rgba ${show ? "translate-x-0" : "translate-x-full"}`}>
             <div className={`w-[50%] flex flex-col p-[15px] h-full bg-white fixed duration-500 ease-out right-0 transition-transform transform ${show ? "translate-x-0" : "translate-x-full"}`}>
-                <h4 className='mb-[30px] text-[15px] cursor-pointer'><Link href="/">Home</Link></h4>
-                <h4 className='mb-[30px] text-[15px] cursor-pointer'><Link href="/about">About</Link></h4>
-                <h4 className='mb-[30px] text-[15px] cursor-pointer'><Link href="/service">Services</Link></h4>
-                <h4 className='mb-[30px] text-[15px] cursor-pointer'><Link href="/blog">Blog</Link></h4>
-                <Link href="/contact">
+                <h4 className='mb-[30px] text-[15px] cursor-pointer'><Link href="/" onClick={closeSidebar}>Home</Link></h4>
+                <h4 className='mb-[30px] text-[15px] cursor-pointer'><Link href="/about" onClick={closeSidebar}>About</Link></h4>
+                <h4 className='mb-[30px] text-[15px] cursor-pointer'><Link href="/service" onClick={closeSidebar}>Services</Link></h4>
+                <h4 className='mb-[30px] text-[15px] cursor-pointer'><Link href="/blog" onClick={closeSidebar}>Blog</Link></h4>
+                <Link href="/contact" onClick={closeSidebar}>
                     <button className='w-[100%] h-[45px] flex justify-center items-center text-[13px] font-[600] border border-border rounded-[10px] cursor-pointer'>Contact Us</button>
                 </Link>
             </div>
